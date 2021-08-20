c85db4e7ff5a31daafd0a267e331d494813995e6
Sean Morgan <scm0323@att.net>
2021-08-20 11:26:01 -0500

    complete Area Of Circle task

---
+++ Area Of Circle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Area_Of_Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {AreaOfCircle(radius)}");
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * radius * radius;
+            return area;
+        }
+    }
+}
